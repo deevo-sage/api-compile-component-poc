48169b5ecfe280978b812f478f10d1d41705ba71
sidharth sahni <54073655+deevo-sage@users.noreply.github.com>
2025-03-17 15:57:53 +0530

    script tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,6 @@ import Link from "next/link";
 import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    fetch("/api/vite-build")
-      .then((res) => res.text())
-      .then((res) => eval(res));
-  }, []);
   return (
     <>
       <Head>
@@ -17,6 +12,7 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div id="nav-root"></div>
+        <script src="/api/vite-build" type="module"></script>
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
